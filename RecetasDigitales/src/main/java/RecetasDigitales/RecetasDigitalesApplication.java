d9803eef5509d7fb2ced55311ac61d18ddae7a86
jorge Rosas <jorge.99rg@gmail.com>
2025-02-25 23:39:13 -0600

    first commit

---
+++ RecetasDigitales/src/main/java/RecetasDigitales/RecetasDigitalesApplication.java
@@ -0,0 +1,13 @@
+package RecetasDigitales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetasDigitalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetasDigitalesApplication.class, args);
+	}
+
+}
---
+++ RecetasDigitales/src/test/java/RecetasDigitales/RecetasDigitalesApplicationTests.java
@@ -0,0 +1,13 @@
+package RecetasDigitales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetasDigitalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
