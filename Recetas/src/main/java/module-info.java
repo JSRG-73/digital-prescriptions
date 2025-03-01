d0a1efd6a1a195e0db397f5e5fedb7dd2bf4c9bf
jorge Rosas <jorge.99rg@gmail.com>
2025-02-28 22:23:45 -0600

    Added prescription template

--- Recetas/src/main/java/module-info.java
+++ Recetas/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.jorgerosas.recetas {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.desktop;
+    requires org.apache.pdfbox;
 
     opens com.jorgerosas.recetas to javafx.fxml;
     exports com.jorgerosas.recetas;
