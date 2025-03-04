50a68f09795f8b32c0e9f980988d0211c48f09d6
jorge Rosas <jorge.99rg@gmail.com>
2025-03-03 23:37:11 -0600

    reorganized folder structure

--- Recetas/src/main/java/module-info.java
+++ Recetas/src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.jorgerosas.recetas {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    requires org.apache.pdfbox;
+    requires itextpdf;
+    //requires org.xhtmlrenderer;
 
     opens com.jorgerosas.recetas to javafx.fxml;
     exports com.jorgerosas.recetas;
