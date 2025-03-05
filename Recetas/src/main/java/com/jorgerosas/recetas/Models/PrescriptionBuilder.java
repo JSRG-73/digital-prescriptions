80cc64fcdb168db816766a2daec659154eb4b921
jorge Rosas <jorge.99rg@gmail.com>
2025-03-05 12:31:23 -0600

    PrescriptionBuilder static functions converted to non-static methods

--- Recetas/src/main/java/com/jorgerosas/recetas/Models/PrescriptionBuilder.java
+++ Recetas/src/main/java/com/jorgerosas/recetas/Models/PrescriptionBuilder.java
@@ -9,32 +9,18 @@ import java.nio.charset.StandardCharsets;
 public class PrescriptionBuilder {
 
     private String html;
-    private String path;
+    private final String path;
 
     public PrescriptionBuilder(String path){
         this.path=path;
     }
 
-    public static void main(String[] args) {
-
-        PrescriptionBuilder pb = new PrescriptionBuilder();
-        String htmlContent = pb.readHtmlFromResources("/templates/UC/UC.html");
-
-
-        if (htmlContent != null) {
-            System.out.println(htmlContent);
-        } else {
-            System.out.println("Archivo no encontrado");
-        }
-    }
-
-    public String replaceDataShort(String html, String patient, String date, String description){
+    public void replaceDataShort(String patient, String date, String description){
 
         html=html.replace("{{patient}}", patient);
         html=html.replace("{{date}}", date);
         html=html.replace("{{description}}", description);
 
-        return html;
     }
 
     public String replaceData(String html, String patient, String date, String description){
@@ -44,20 +30,37 @@ public class PrescriptionBuilder {
         return filledhtml.toString();
     }
 
-    public String readHtmlFromResources(String filePath) {   //Converts html file to java String
+    public void readHtmlFromResources() {   //Converts html file to java String
 
         StringBuilder content = new StringBuilder();
         String line;
 
-        try (InputStream inputStream = PrescriptionBuilder.class.getResourceAsStream(filePath);
+        try (InputStream inputStream = PrescriptionBuilder.class.getResourceAsStream(this.path);
              BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
 
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
-            }
+            while ((line = reader.readLine()) != null) {content.append(line).append("\n");}
 
-        } catch (IOException | NullPointerException e) { return null; }
+        } catch (IOException | NullPointerException e) {
+            System.out.println(e); }
 
-        return content.toString();
+        this.html = content.toString();
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+    public static void main(String[] args) {
+
+        PrescriptionBuilder pb = new PrescriptionBuilder("/templates/UC/UC.html");
+        pb.readHtmlFromResources();
+        pb.replaceDataShort("Jorge Rosas", "12/12/12", "Descripción");
+        String htmlContent = pb.getHtml();
+
+        if (htmlContent != null) {
+            System.out.println(htmlContent);
+        } else {
+            System.out.println("Archivo no encontrado");
+        }
     }
 }
