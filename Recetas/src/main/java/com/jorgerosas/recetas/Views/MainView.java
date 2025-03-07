76c4a1893595858a9d891ede48e509c4a872c580
jorge Rosas <jorge.99rg@gmail.com>
2025-03-06 23:24:10 -0600

    MainView added

---
+++ Recetas/src/main/java/com/jorgerosas/recetas/Views/MainView.java
@@ -0,0 +1,22 @@
+package com.jorgerosas.recetas.Views;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class MainView extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(com.jorgerosas.recetas.HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
