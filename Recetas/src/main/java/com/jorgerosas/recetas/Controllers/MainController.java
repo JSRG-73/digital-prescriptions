92f1fe47b2de6c82250b267c0b49a4b726461602
jorge Rosas <jorge.99rg@gmail.com>
2025-03-08 23:15:38 -0600

    Main Controller fxml file added

---
+++ Recetas/src/main/java/com/jorgerosas/recetas/Controllers/MainController.java
@@ -0,0 +1,12 @@
+package com.jorgerosas.recetas.Controllers;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+
+public class MainController {
+
+    @FXML
+    private Label t1;
+
+}
