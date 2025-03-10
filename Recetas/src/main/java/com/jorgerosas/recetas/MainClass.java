813d65ed72155b22355f296f29a4b39b97cf5e35
jorge Rosas <jorge.99rg@gmail.com>
2025-03-09 23:09:23 -0600

    Entrypoint class updated

--- Recetas/src/main/java/com/jorgerosas/recetas/MainClass.java
+++ Recetas/src/main/java/com/jorgerosas/recetas/MainClass.java
@@ -1,7 +1,24 @@
 package com.jorgerosas.recetas;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class MainClass {
-    public static void main(String[] args){
+import java.io.IOException;
 
+public class MainClass extends Application{
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main-view.fxml"));
+        AnchorPane root = loader.load();
+
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setTitle("My JavaFX App");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
