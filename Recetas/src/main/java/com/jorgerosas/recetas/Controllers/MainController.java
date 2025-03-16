cdf2dd3f11412afaf7ee46c867c71dbc83984649
jorge Rosas <jorge.99rg@gmail.com>
2025-03-15 22:52:20 -0600

    MainController updated

--- Recetas/src/main/java/com/jorgerosas/recetas/Controllers/MainController.java
+++ Recetas/src/main/java/com/jorgerosas/recetas/Controllers/MainController.java
@@ -1,12 +1,17 @@
 package com.jorgerosas.recetas.Controllers;
 
 import javafx.fxml.FXML;
-
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainController {
 
+    @FXML private Label messageLabel;
+    @FXML private Button clickButton;
+
     @FXML
-    private Label t1;
+    private void initialize() {
+        clickButton.setOnAction(e -> messageLabel.setText("Button Clicked!"));
+    }
 
 }
