bb77eb5cabb0626a5212261019a5f554f105e24a
jorge Rosas <jorge.99rg@gmail.com>
2025-03-22 23:37:13 -0600

    Simpler css for UC template

--- Recetas/src/main/java/com/jorgerosas/recetas/Models/PrescriptionBuilder.java
+++ Recetas/src/main/java/com/jorgerosas/recetas/Models/PrescriptionBuilder.java
@@ -1,8 +1,19 @@
 package com.jorgerosas.recetas.Models;
 
-import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Pdf;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.print.PrintOptions;
+import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class PrescriptionBuilder {
 
@@ -53,61 +64,6 @@ public class PrescriptionBuilder {
         } catch (IOException e) {
             System.out.println("Error writing HTML file: " + e.getMessage());
         }
-    }
-
-    public void generatePDF(String filePath) {
-        try {
-            // Create parent directories if they don't exist
-            File dir = new File(filePath).getParentFile();
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
-
-            // Fix HTML to ensure proper XML compliance and add print-specific styles
-            String fixedHtml = fixHtmlForXmlCompliance(html);
-
-            // Create an output stream for the PDF
-            try (OutputStream os = new FileOutputStream(filePath)) {
-                // Create a PDF renderer builder
-                PdfRendererBuilder builder = new PdfRendererBuilder();
-
-                // Get the base URI for resolving resources
-                String baseUri = new File(filePath).getParentFile().toURI().toString();
-
-                // Set the HTML content with base URI
-                builder.withHtmlContent(fixedHtml, baseUri);
-                
-                // Set output stream
-                builder.toStream(os);
-
-                // Run the renderer
-                builder.run();
-
-                System.out.println("PDF file generated successfully at: " + filePath);
-            }
-        } catch (Exception e) {
-            System.out.println("Error generating PDF file: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    private String fixHtmlForXmlCompliance(String html) {
-        // Replace standalone <br> tags with self-closing <br/> tags
-        html = html.replaceAll("<br>", "<br/>");
-        html = html.replaceAll("<br\\s+>", "<br/>");
-
-        // Replace other common HTML elements that might not be properly closed
-        html = html.replaceAll("<hr>", "<hr/>");
-        html = html.replaceAll("<img([^>]*)>", "<img$1/>");
-        html = html.replaceAll("<input([^>]*)>", "<input$1/>");
-
-        // Make sure meta tags are properly closed
-        html = html.replaceAll("<meta([^>]*)>", "<meta$1/>");
-
-        // Make sure link tags are properly closed
-        html = html.replaceAll("<link([^>]*)>", "<link$1/>");
-
-        return html;
     }
 
     public static void main(String[] args) {
