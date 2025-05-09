6421608f4fb5c228b3c4050e07765ad110ead71c
Jorge Rosas <jorge.99rg@gmail.com>
2025-05-08 23:16:41 -0600

    Hover fixed and .content height changed to 35lh in styles.css

--- Recetas/src/main/java/com/jorgerosas/recetas/Models/InfoDialog.java
+++ Recetas/src/main/java/com/jorgerosas/recetas/Models/InfoDialog.java
@@ -4,6 +4,7 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class InfoDialog {
     public static void showInfo(){
@@ -15,23 +16,26 @@ public class InfoDialog {
         VBox content = new VBox();
         content.setSpacing(10);
 
-        Text text1 = new Text("Recetario digital desarrollado para el Dr. EDUARDO BARRAGÁN CURIEL");
-        text1.setStyle("-fx-font-weight: bold;"); // Keeps default font family/size
+        Text normalText = new Text("Recetario digital desarrollado para el ");
+        Text boldName = new Text("Dr. Eduardo Barragán Curiel");
+        boldName.setStyle("-fx-font-weight: bold;");
+
+        TextFlow textFlow = new TextFlow(normalText, boldName);
 
         content.getChildren().addAll(
-                text1,
-                new Text("Version: 1.5.6"),
+                textFlow,
+                new Text("Versión: 1.5.7"),
                 new Text(""),
                 new Text("Guía rápida:"),
                 new Text("-En 'Nueva Receta' puede generar una receta introduciendo el nombre y la descripción que esta va a tener,"),
                 new Text("  el apartado 'fecha' carga la fecha de la computadora en automático, no es necesario introducirla, a no "),
                 new Text("  ser que el usuario así lo quiera. "),
-                new Text("-Al presionar 'Generar PDF', la receta se guardará y se generará en forma de PDF (esto puede tardar unos segundos)"),
+                new Text("-Al presionar 'Generar PDF', la receta se guardará y se generará en formato PDF (esto puede tardar unos segundos)"),
                 new Text("  con la plantilla de la Unidad de Cancerología. "),
-                new Text("-Para ver la carpeta con todos los PDF's presione el botón 'Ver Recetas'."),
+                new Text("-Para ver la carpeta con todos los archivos PDF presione el botón 'Ver Recetas'."),
                 new Text("-En 'Cargar Receta' puede cargar los datos de recetas previas para reutilizar la descripción de la misma,"),
                 new Text("  puede buscar recetas previas por nombre o fecha en el apartado de 'buscar', para cargar o eliminar recetas,"),
-                new Text("-Al eliminar una receta NO borrará el PDF, solo no estará disponible para su reutilización."),
+                new Text("-Al eliminar una receta NO borrará el archivo PDF, solo no estará disponible para su reutilización."),
                 new Text("-No hay límite de texto para el campo Descripción, solo para el campo Nombre"),
                 new Text("-Para guardar el archivo PDF de forma segura, el programa remplaza por un guión bajo _ caracteres como los"),
                 new Text(" siguientes: “ ” ‘ ’ ( ) [ ] { } ‹ › + − × ÷ = ≠ ± < > $, €, ¥, ₹, ₿ , ; :"),
